1f86b71129e9149b9f21e901ffcca99c75d8401a
Tatiana Belova <tatyana.belova@ligastavok.ru>
2021-03-16 11:56:14 +0400

    Add splash and app icon, change app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'file:///C:/Users/tatiana/StudioProjects/FlutterExamples/lib/lesson%205/bloc_library/bloc/observer.dart';
-import 'file:///C:/Users/tatiana/StudioProjects/FlutterExamples/lib/lesson%205/bloc_library/presentation/coins_bloc_library_page.dart';
 
-import 'lesson 4/coins/widgets/coins_page.dart';
-import 'lesson 5/bloc_library/bloc/coin_bloc.dart';
 import 'lesson 5/bloc_pattern/coins_pattern_page.dart';
 
 void main() {
